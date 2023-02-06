fb6e3f9c67666e4a3f2696a092208af68b55be6e
Kiran <krnsundal@gmail.com>
2023-02-06 13:03:00 +0000

    current location bug

--- project1/libs/js/map.js
+++ project1/libs/js/map.js
@@ -1,5 +1,20 @@
-console.log("Map Connected");
-var map = L.map("map").setView([51.505, -0.09], 6);
+// Getting current location
+function getGeoeolocation() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("position in geolocation", position);
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log("Latitude:" + lat + "InLongitude: " + lon);
+      let location = [];
+      location.push(lat, lon);
+      return location;
+    });
+  }
+}
+let view = getGeoeolocation();
+let marker;
+var map = L.map("map").setView(view, 6);
 
 var MapTilesAPI_OSMEnglish = L.tileLayer(
   "https://maptiles.p.rapidapi.com/en/map/v1/{z}/{x}/{y}.png?rapidapi-key=c4edb04533mshba882524ef1f0e1p1f0643jsna3c2c78e057f",
@@ -14,21 +29,32 @@ var MapTilesAPI_OSMEnglish = L.tileLayer(
 //easy buttons
 // Triggers basic Contry Infomatiobn Modal
 L.easyButton("fa-globe", function (btn, map) {
-  let basicCountryImfo = new bootstrap.Modal($("#countryInfo"), {});
-
-  basicCountryImfo.show();
+  if ($("#selectCountries").val() === "") {
+    alert("Please Select a Country");
+  } else {
+    let basicCountryImfo = new bootstrap.Modal($("#countryInfo"), {});
+    basicCountryImfo.show();
+  }
 }).addTo(map);
 
 // Triggers basic Weather  Modal
 L.easyButton("fa-cloud", function (btn, map) {
-  let weatherModal = new bootstrap.Modal($("#weatherInfo"), {});
-
-  weatherModal.show();
+  if ($("#selectCountries").val() === "") {
+    alert("Please Select a Country");
+  } else {
+    let weatherModal = new bootstrap.Modal($("#weatherInfo"), {});
+    weatherModal.show();
+  }
 }).addTo(map);
 
 // Triggers useful information  Modal
 L.easyButton("fa-circle-info", function (btn, map) {
-  let usefulInfoModal = new bootstrap.Modal($("#usefulInfo"), {});
-
-  usefulInfoModal.show();
+  if ($("#selectCountries").val() === "") {
+    alert("Please Select a Country");
+  } else {
+    let usefulInfoModal = new bootstrap.Modal($("#usefulInfo"), {});
+    usefulInfoModal.show();
+  }
 }).addTo(map);
+
+marker = L.marker(view).addTo(map);
--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,10 +1,13 @@
-$(document).ready(function () {
+$(document).ready(() => {
+  
+
   // Opening modal
   $("#appInstructions").modal("show");
   let allCountries = [];
   let allRestCountries = [];
+  let polygons = [];
 
-  // ajaz request functions
+  // ajax request functions
   const getCountries = () => {
     $.ajax({
       type: "GET",
@@ -12,12 +15,11 @@ $(document).ready(function () {
       data: "",
       dataType: "json",
       success: function (response) {
-        // console.log("Countries from json", response.features);
-
-        let countryInmfo = response.features;
-        allCountries = countryInmfo;
+        let countryInfo = response.features;
+        allCountries = countryInfo;
+        allCountries.sort();
         let str = "";
-        countryInmfo.forEach((country) => {
+        countryInfo.forEach((country) => {
           // console.log('',country);
           str += `<option value="${country.properties.name}">${country.properties.name}</option>`;
         });
@@ -51,7 +53,7 @@ $(document).ready(function () {
       data: { lat: lat, long: long },
       dataType: "json",
       success: function (response) {
-        console.log("weather response", response);
+        // console.log("weather response", response);
         weatherInfo = response;
         let t = (parseFloat(response.main.temp) - 273.15).toFixed(2);
 
@@ -76,25 +78,94 @@ $(document).ready(function () {
       data: { currency: currency },
       dataType: "json",
       success: function (response) {
-        console.log("exchange rate response", response);
+        // console.log("exchange rate response", response);
         let str = "";
         const usd = response.rates.USD;
-        const gbp = response.data.GBP;
+        const gbp = response.rates.GBP;
         const eur = response.rates.EUR;
-        console.log("usd", usd);
-        console.log("gbp", gbp);
-        console.log("eur", eur);
 
-        // if(response.rates.rate === "USD"&&)
-        // str += `<li class="rate-li"><span clas="currency"></span>
-        // <span clas=rate"></span>
-        // </li>`;
+        str += `<li class="rate-li"><span class="currency">$(USD)</span>
+        <span id=usd">${usd}</span>
+        <li class="rate-li"><span class="currency">€(EUR)</span>
+        <span id=eur">${eur}</span>
+        <li class="rate-li"><span class="currency">£(GBP)</span>
+        <span id=gbp">${gbp}</span>
+        </li>`;
+        $("#u-result-1").html(str);
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log("err ");
         console.log("Error", errorThrown, jqXHR);
       },
     });
+  };
+  const getWikiLinks = (lat, long) => {
+    $.ajax({
+      type: "GET",
+      url: "libs/php/getWikiLinks.php",
+      data: { lat: lat, long: long },
+      dataType: "json",
+      success: function (response) {
+        console.log("response from getWikiLinks", response);
+        if (response === []) {
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log("Error", errorThrown, jqXHR);
+      },
+    });
+  };
+  const drawBorders = (country) => {
+    console.log("Draw bordfers func", country);
+    let countryGeometry = country.geometry;
+    let type = countryGeometry.type;
+    let correctCoords = [];
+    if (type === "MultiPolygon") {
+      let multiPolygonCoords = countryGeometry.coordinates;
+      console.log("multiPolygonCoords inner first element", multiPolygonCoords);
+      for (let i = 0; i < multiPolygonCoords.length; i++) {
+        correctCoords = [];
+        for (let j = 0; j < multiPolygonCoords[i][0].length; j++) {
+          let first = multiPolygonCoords[i][0][j][1];
+          let second = multiPolygonCoords[i][0][j][0];
+          let tempCords = [];
+          tempCords.push(first, second);
+          correctCoords.push(tempCords);
+          // console.log("multipolygon temp:", tempCords);
+        }
+        let polygon = L.polygon(correctCoords, {
+          color: "red",
+          fillColor: "#f03",
+        }).addTo(map);
+        polygons.push(polygon);
+      }
+    } else if (type === "Polygon") {
+      // console.log(country, "I am a polygon");
+      countryGeometry.coordinates[0].forEach((coord) => {
+        console.log(coord);
+        let first = coord[1];
+        let second = coord[0];
+        let tempCords = [];
+        tempCords.push(first, second);
+        correctCoords.push(tempCords);
+      });
+
+      //Drawing border
+      let polygon = L.polygon(correctCoords, {
+        color: "red",
+        fillColor: "#f03",
+      }).addTo(map);
+      polygons.push(polygon);
+    }
+  };
+
+  const removeBorders = () => {
+    for (let i = 0; i < polygons.length; i++) {
+      const element = polygons[i];
+      if (element !== null) {
+        map.removeLayer(element);
+      }
+    }
   };
   //Calling getCountries to populate select
   getCountries();
@@ -104,6 +175,9 @@ $(document).ready(function () {
   $("#selectCountries").change(() => {
     let selectval = $("#selectCountries").val();
 
+    if (polygons !== undefined) {
+      removeBorders();
+    }
     //find t he name in geoCountries that matches select dropdown
     const singleCountry = allCountries.find((c) => {
       return c.properties.name == selectval;
@@ -113,30 +187,51 @@ $(document).ready(function () {
       (restCountry) => singleCountry.properties.iso_a3 === restCountry.cca3
     );
     //Displayting data in basic Info modal
-    console.log("SingleCountry from restCountry", singleRestCountry);
-    $("#result-1").html(singleRestCountry.name.common);
-    $("#result-2").html(singleRestCountry.capital[0]);
-    $("#result-3").html(singleRestCountry.population);
+    // console.log("SingleCountry from restCountry", singleRestCountry);
+    console.log("SingleCountry", singleCountry);
+    if (singleRestCountry) {
+      $("#result-1").html(singleRestCountry.name.common);
+      $("#result-2").html(singleRestCountry.capital[0]);
+      $("#result-3").html(singleRestCountry.population);
+      $("#result-4").html(
+        `<img class="flag" src="${singleRestCountry.flags.png}"/>`
+      );
+      if (singleRestCountry.borders) {
+        let str = "";
+        console.log("borders", singleRestCountry.borders);
+        singleRestCountry.borders.forEach((border) => {
+          str += `<li>${border}</li>`;
+        });
+        $("#result-5").html(str);
+      } else {
+        $("#result-5").html("Borders Not Found");
+      }
+      if (singleRestCountry.subregion) {
+        $("#result-6").html(singleRestCountry.subregion);
+      } else {
+        $("#result-6").html("Subregion Not Found");
+      }
 
-    $("#result-4").html(
-      `<img class="flag" src="${singleRestCountry.flags.png}"/>`
-    );
+      let lat = singleRestCountry.latlng[0];
+      let long = singleRestCountry.latlng[1];
+      getWeatherInfo(lat, long);
+      // Populating Weather modal
+      $("#w-result-4").html(lat);
+      $("#w-result-5").html(long);
+      let countryCurrency = Object.keys(singleRestCountry.currencies)[0];
+      // calling getExchangeRate for useful info modal
+      getExchangeRate(countryCurrency);
+      getWikiLinks(lat, long);
+      //makes maker jump to country
+      if (marker !== null) {
+        map.removeLayer(marker);
+      }
+      map.panTo([lat, long], { animate: true, duration: 1 });
+      marker = L.marker([lat, long]).addTo(map);
 
-    let str = "";
-    singleRestCountry.borders.forEach((border) => {
-      str += `<li>${border}</li>`;
-    });
-    $("#result-5").html(str);
-    $("#result-6").html(singleRestCountry.subregion);
-
-    let lat = singleRestCountry.latlng[0];
-    let long = singleRestCountry.latlng[1];
-    getWeatherInfo(lat, long);
-    // Populating Weather modal
-    $("#w-result-4").html(lat);
-    $("#w-result-5").html(long);
-    let countryCurrency = Object.keys(singleRestCountry.currencies)[0];
-    // calling getExchangeRate for useful info modal
-    getExchangeRate(countryCurrency);
+      drawBorders(singleCountry);
+    } else {
+      alert("Country Not Found");
+    }
   });
 });

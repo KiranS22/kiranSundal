19766bf7cc9c6d39a6ef73284810e88e99adb30c
Kiran <krnsundal@gmail.com>
2023-03-03 13:36:23 +0000

    North Cyprus Error handled

--- project1/libs/php/getCountries.php
+++ project1/libs/php/getCountries.php
@@ -3,9 +3,13 @@
 if(isset($_REQUEST['iso'])){
   $countries = json_decode(file_get_contents("countryBorders.geo.json"), true);
   foreach ($countries['features'] as $country){
+
     if($country['properties']['iso_a2'] ==$_REQUEST['iso'] ){
       echo json_encode($country);
 
+      if($country['properties']['iso_a2'] == '-99'){
+        continue;
+      }  
     }
   }
 

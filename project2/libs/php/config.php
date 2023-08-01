02642ee6245f560437022bd6dfd8e6c0000410db
Kiran <krnsundal@gmail.com>
2023-08-01 11:21:41 +0100

    Moved Porfolio to seperate repo

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -1,7 +1,17 @@
 
 <?php
-// database name, username and password
 
-	$cd_dbname = "";
-	$cd_user = "";
-	$cd_password = "";
+	// // connection details for MySQL database
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// // database name, username and password
+
+	$cd_dbname = "u398116924_company";
+	$cd_user = "u398116924_db_user";
+	$cd_password = "Kiran123";
+
+
+?>

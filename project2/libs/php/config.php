de9365e05a1bff1ed0f716625d693a0e8e790809
Kiran <krnsundal@gmail.com>
2023-08-01 10:30:17 +0100

    updated project2 config

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -1,21 +1,7 @@
 
 <?php
+// database name, username and password
 
-	// // connection details for MySQL database
-
-	$cd_host = "127.0.0.1";
-	$cd_port = 3306;
-	$cd_socket = "";
-
-	// // database name, username and password
-
-	$cd_dbname = "u398116924_company";
-	$cd_user = "u398116924_db_user";
-	$cd_password = "Kiran123";
-
-	// For local changes if needed
-
-	// $cd_dbname = "companydirectory";
-	// $cd_user = "root";
-	// $cd_password = "";
-?>
+	$cd_dbname = "";
+	$cd_user = "";
+	$cd_password = "";
